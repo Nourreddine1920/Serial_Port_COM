75ac1be0bca260a131a657245b296106066fe1cb
Noureddine AWLED BRAHIM <noureddine.awledbrahim@actia-engineering.tn>
2023-02-27 17:09:54 +0100

    adding I2C & SPI Configurations

--- dashboard.cpp
+++ dashboard.cpp
@@ -9,19 +9,78 @@ Dashboard::Dashboard(QWidget *parent) :
     QWidget::setWindowTitle("IPs Configurations ");
 
     auto *quit = new QAction("&Quit", this);
+
+    // Checkable UART Configurations
     QAction *UART4 = new QAction("UART4", this);
     UART4->setCheckable(true);
 
-    //auto *UART4 = new QAction("&UART4", this);
-    auto *UART5 = new QAction("&UART5", this);
-    auto *UART7 = new QAction("&UART7", this);
-    auto *UART8 = new QAction("&UART8", this);
-    auto *UART9 = new QAction("&UART9", this);
-    auto *USART1 = new QAction("&USART1", this);
 
-    auto *USART2 = new QAction("&USART2", this);
-    auto *USART6 = new QAction("&USART6", this);
-    auto *USART10 = new QAction("&USART10", this);
+    QAction *UART5 = new QAction("&UART5", this);
+    UART5->setCheckable(true);
+
+    QAction *UART7 = new QAction("&UART7", this);
+    UART7->setCheckable(true);
+
+    QAction *UART8 = new QAction("&UART8", this);
+    UART8->setCheckable(true);
+
+    QAction *UART9 = new QAction("&UART9", this);
+    UART9->setCheckable(true);
+
+    QAction *USART1 = new QAction("&USART1", this);
+    USART1->setCheckable(true);
+
+
+    QAction *USART2 = new QAction("&USART2", this);
+    USART2->setCheckable(true);
+
+    QAction *USART6 = new QAction("&USART6", this);
+    USART6->setCheckable(true);
+
+    QAction *USART10 = new QAction("&USART10", this);
+    USART10->setCheckable(true);
+
+
+
+    // Checkable SPI Configurations
+    QAction *SPI1 = new QAction("SPI1", this);
+    SPI1->setCheckable(true);
+
+
+    QAction *SPI2 = new QAction("&SPI2", this);
+    SPI2->setCheckable(true);
+
+    QAction *SPI3 = new QAction("&SPI3", this);
+    SPI3->setCheckable(true);
+
+    QAction *SPI4 = new QAction("&SPI4", this);
+    SPI4->setCheckable(true);
+
+    QAction *SPI5 = new QAction("&SPI5", this);
+    SPI5->setCheckable(true);
+
+    QAction *SPI6 = new QAction("&SPI6", this);
+    SPI6->setCheckable(true);
+
+    // Checkable I2C Configurations
+    QAction *I2C1 = new QAction("I2C1", this);
+    I2C1->setCheckable(true);
+
+
+    QAction *I2C2 = new QAction("&I2C2", this);
+    I2C2->setCheckable(true);
+
+    QAction *I2C3 = new QAction("&I2C3", this);
+    I2C3->setCheckable(true);
+
+    QAction *I2C4 = new QAction("&I2C4", this);
+    I2C4->setCheckable(true);
+
+    QAction *I2C5 = new QAction("&I2C5", this);
+    I2C5->setCheckable(true);
+
+
+
 
 
 
@@ -33,7 +92,7 @@ Dashboard::Dashboard(QWidget *parent) :
     QMenu *DAC = menuBar()->addMenu("&DAC Selection");
     QMenu *TIMER = menuBar()->addMenu("&TIMER Selection");
     QFont font = menuBar()->font();
-    font.setPointSize(font.pointSize() + 2); // increase by 2 points
+    font.setPointSize(font.pointSize() + 6); // increase by 2 points
     font.setPixelSize(20);
     font.setPointSizeF(10);
     QSizePolicy policy = UART->sizePolicy();
@@ -41,7 +100,7 @@ Dashboard::Dashboard(QWidget *parent) :
     UART->setSizePolicy(policy);
     menuBar()->setFont(font);
     UART->setFont(font);
-
+    // Available UART IPs
     UART->addAction(UART4);
     UART->addAction(UART5);
     UART->addAction(UART7);
@@ -53,7 +112,66 @@ Dashboard::Dashboard(QWidget *parent) :
     UART->addAction(USART6);
     UART->addAction(USART10);
     TIMER->addAction(USART1);
+
+    // Connect to the UART configurations
     connect(UART4, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(UART5, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(UART7, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(UART8, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(UART9, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(USART1, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(USART2, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(USART6, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(USART10, &QAction::triggered, this, &Dashboard::openWidget);
+
+
+    // Available SPI IPs
+    SPI->addAction(SPI1);
+    SPI->addAction(SPI2);
+    SPI->addAction(SPI3);
+    SPI->addAction(SPI4);
+    SPI->addAction(SPI5);
+
+    SPI->addAction(SPI6);
+
+
+    // Connect to the SPI configurations
+    connect(SPI1, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(SPI2, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(SPI3, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(SPI4, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(SPI5, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(SPI6, &QAction::triggered, this, &Dashboard::openWidget);
+
+
+    // Available I2C IPs
+    I2C->addAction(I2C1);
+    I2C->addAction(I2C2);
+    I2C->addAction(I2C3);
+    I2C->addAction(I2C4);
+    I2C->addAction(I2C5);
+
+
+
+    // Connect to the SPI configurations
+    connect(I2C1, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(I2C2, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(I2C3, &QAction::triggered, this, &Dashboard::openWidget);
+    connect(I2C4, &QAction::triggered, this, &Dashboard::openWidget);
+
+    connect(I2C5, &QAction::triggered, this, &Dashboard::openWidget);
+
+
+
+
+
     connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
 
--- dashboard.h
+++ dashboard.h
@@ -27,42 +27,11 @@ private slots :
         QWidget *widget = new QWidget(this);
         setCentralWidget(widget);
 
-
-        // Baudrate  configurations
-
-//        QLabel *Baudrate = new QLabel("Baudrate : ", widget);
-//        QComboBox *BaudrateList = new QComboBox;
-//        QList<qint32> baudRates = info.standardBaudRates(); // What baudrates does my computer support ?
-//        QList<QString> stringBaudRates;
-//        for(int i = 0 ; i < baudRates.size() ; i++){
-//            stringBaudRates.append(QString::number(baudRates.at(i)));
-//        }
-
-//        BaudrateList->addItems(stringBaudRates);
-//        QFormLayout* layout = new QFormLayout(this);
-
-//        Baudrate->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-//        BaudrateList->setStyleSheet("font: 15px; color: blue; background-color: white;");
-
-//        layout->addRow(Baudrate, BaudrateList);
-
-//        //widget->setLayout(layout);
-
-
-//        // Parity configurations
-//        QLabel *Parity = new QLabel(tr("Parity : "), widget);
-//        QComboBox *ParityList = new QComboBox;
-
-//        ParityList->addItem("No Parity");
-//        ParityList->addItem("Odd Parity");
-//        ParityList->addItem("Even Parity");
-//        Parity->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-//        ParityList->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-//        layout->addRow(Parity, ParityList);
-
+                // Create Layout form
                 QFormLayout* layout = new QFormLayout(this);
 
                // Create the baud rate label and combo box
+
                QLabel* baudRateLabel = new QLabel(tr("Baud Rate"), this);
                QComboBox* baudRateComboBox = new QComboBox(this);
                QList<qint32> baudRates = info.standardBaudRates(); // What baudrates does my computer support ?
@@ -75,34 +44,42 @@ private slots :
                // Set the minimum width to 100 pixels
                baudRateComboBox->setMinimumWidth(10);
                baudRateLabel->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-               baudRateComboBox->setStyleSheet("font: 15px; color: blue; background-color: white;");
+               baudRateComboBox->setStyleSheet("font: 15px; color: green; background-color: white;");
 
 
                layout->addRow(baudRateLabel, baudRateComboBox);
 
                // Create the parity label and combo box
+
                QLabel* parityLabel = new QLabel(tr("Parity"), this);
                QComboBox* parityComboBox = new QComboBox(this);
                parityComboBox->addItems(QStringList() << "None" << "Odd" << "Even");
                parityLabel->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-               parityComboBox->setStyleSheet("font: 15px; color: blue; background-color: white;");
+               parityComboBox->setStyleSheet("font: 15px; color: green; background-color: white;");
 
                layout->addRow(parityLabel, parityComboBox);
 
+
                // Create the stop bits label and combo box
+
                QLabel* stopBitsLabel = new QLabel(tr("Stop Bits"), this);
                QComboBox* stopBitsComboBox = new QComboBox(this);
                stopBitsComboBox->addItems(QStringList() << "1" << "1.5" << "2");
                stopBitsLabel->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-               stopBitsComboBox->setStyleSheet("font: 15px; color: blue; background-color: white;");
+               stopBitsComboBox->setStyleSheet("font: 15px; color: green; background-color: white;");
 
                layout->addRow(stopBitsLabel, stopBitsComboBox);
+
+
+               // Create the data bits label and combo box
 
                QLabel* DataBitsLabel = new QLabel(tr("Data Bits"), this);
                QComboBox* DataBitsComboBox = new QComboBox(this);
                DataBitsComboBox->addItems(QStringList() << "5" << "6" << "7" << "8");
                DataBitsLabel->setStyleSheet("font: bold 15px; color: black; background-color: white;");
-               DataBitsComboBox->setStyleSheet("font: 15px; color: blue; background-color: white;");
+               DataBitsComboBox->setStyleSheet("font: 15px; color: green; background-color: white;");
+
+               // Create the flow control label and combo box
 
                layout->addRow(DataBitsLabel, DataBitsComboBox);
                QLabel* FlowControlLabel = new QLabel(tr("Flow Control"), this);
@@ -114,20 +91,20 @@ private slots :
                layout->addRow(FlowControlLabel, FlowControlComboBox);
 
                // Create the vertical layout and add the form layout to it
-                      QVBoxLayout* verticalLayout = new QVBoxLayout(this);
-                      verticalLayout->addStretch();
-                      verticalLayout->addLayout(layout);
-                      verticalLayout->addStretch();
+               QVBoxLayout* verticalLayout = new QVBoxLayout(this);
+               verticalLayout->addStretch();
+               verticalLayout->addLayout(layout);
+               verticalLayout->addStretch();
 
-                      // Create the horizontal layout and add the vertical layout to it
-                      QHBoxLayout* horizontalLayout = new QHBoxLayout(this);
-                      horizontalLayout->addStretch();
-                      horizontalLayout->addLayout(verticalLayout);
-                      horizontalLayout->addStretch();
+               // Create the horizontal layout and add the vertical layout to it
+               QHBoxLayout* horizontalLayout = new QHBoxLayout(this);
+               horizontalLayout->addStretch();
+               horizontalLayout->addLayout(verticalLayout);
+               horizontalLayout->addStretch();
 
-                      // Set the widget layout to the horizontal layout
-                      //setLayout(horizontalLayout);
-                      widget->setLayout(horizontalLayout);
+               // Set the widget layout to the horizontal layout
+
+               widget->setLayout(horizontalLayout);
 
 
 
