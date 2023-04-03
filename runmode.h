5e83e580c0341e1329c1ec5e61443c73e2547663
Nourreddine1920 <Nourreddine1920@github.com>
2023-04-03 14:49:35 +0200

    configuring run ui for SPI IP

--- runmode.h
+++ runmode.h
@@ -9,6 +9,7 @@
 #include <QLabel>
 #include <QCheckBox>
 #include <QRadioButton>
+#include <QComboBox>
 
 namespace Ui {
 class Runmode;
@@ -102,55 +103,109 @@ private slots :
 
 
     void showSPI1Run(){
-        QWidget *widget = new QWidget(this);
-        setCentralWidget(widget);
+        // Create a widget to hold the SPI IP functionality
+        QWidget* spiWidget = new QWidget(this);
+        setCentralWidget(spiWidget);
 
-        auto textBrowser = new QTextBrowser(widget);
-        auto label = new QLabel("Send SPI Command:", widget);
-        auto lineEdit = new QLineEdit(widget);
-        auto button1 = new QPushButton("Send", widget);
-        auto modeLabel = new QLabel("Mode:", widget);
-        auto modeRadio1 = new QRadioButton("Mode 0", widget);
-        auto modeRadio2 = new QRadioButton("Mode 1", widget);
-        auto modeRadio3 = new QRadioButton("Mode 2", widget);
-        auto modeRadio4 = new QRadioButton("Mode 3", widget);
-        auto csCheckBox = new QCheckBox("Chip Select", widget);
-        auto otherPinCheckBox = new QCheckBox("Other Pin", widget);
+        // Create controls for configuring the SPI interface
+        QLabel* DataOrderLabel = new QLabel("Data Order:");
+//        QLineEdit* frequencyEdit = new QLineEdit("1000000");
+        QComboBox* dataOrderCombo = new QComboBox;
+        dataOrderCombo->addItem("MSB First");
+        dataOrderCombo->addItem("LSB First");
+        QLabel* BitOrderLabel = new QLabel("Bits Order:");
 
-        // Create a vertical layout for the text browser, message input, and buttons
-        auto layout = new QVBoxLayout(widget);
-        layout->addWidget(textBrowser);
-        layout->addWidget(label);
-        layout->addWidget(lineEdit);
+        QComboBox* bitOrderCombo = new QComboBox;
+        bitOrderCombo->addItem("MSB First");
+        bitOrderCombo->addItem("LSB First");
 
-        auto buttonLayout = new QHBoxLayout();
-        buttonLayout->addWidget(button1);
-        layout->addLayout(buttonLayout);
+        // Create controls for sending and receiving data
+        QLabel* sendDataLabel = new QLabel("Send Data:");
+        QLineEdit* sendDataEdit = new QLineEdit;
+        QLabel* receivedDataLabel = new QLabel("Received Data:");
+        QTextBrowser* receivedDataBrowser = new QTextBrowser;
 
-        auto optionsLayout = new QGridLayout();
-        optionsLayout->addWidget(modeLabel, 0, 0);
-        optionsLayout->addWidget(modeRadio1, 0, 1);
-        optionsLayout->addWidget(modeRadio2, 0, 2);
-        optionsLayout->addWidget(modeRadio3, 1, 1);
-        optionsLayout->addWidget(modeRadio4, 1, 2);
-        optionsLayout->addWidget(csCheckBox, 2, 0);
-        optionsLayout->addWidget(otherPinCheckBox, 2, 1);
-        layout->addLayout(optionsLayout);
+        // Create buttons for sending and receiving data
+        QPushButton* sendButton = new QPushButton("Send");
+        QPushButton* receiveButton = new QPushButton("Receive");
 
-        // Set some styling properties for the text browser, message input, and buttons
-        textBrowser->setStyleSheet("QTextBrowser { background-color: #f0f0f0; }");
-        lineEdit->setStyleSheet("QLineEdit { background-color: #ffffff; }");
-        button1->setStyleSheet("QPushButton { background-color: #33b5e5; color: white; }");
+        // Create a layout for the widget
+        QVBoxLayout* layout = new QVBoxLayout(spiWidget);
+        layout->addWidget(DataOrderLabel);
+        layout->addWidget(dataOrderCombo);
+        layout->addWidget(BitOrderLabel);
 
-        // Connect the "send message" button to a slot
-        connect(button1, &QPushButton::clicked, [=]() {
-            QString message = lineEdit->text();
-            textBrowser->append(message);
-            lineEdit->clear();
+        layout->addWidget(bitOrderCombo);
+        layout->addWidget(sendDataLabel);
+        layout->addWidget(sendDataEdit);
+        layout->addWidget(receivedDataLabel);
+        layout->addWidget(receivedDataBrowser);
+        layout->addWidget(sendButton);
+        layout->addWidget(receiveButton);
+
+        // Set some styling properties for the controls
+
+        QString styleSheet2 =
+            "QPushButton {"
+            "    background-color: gray;"
+            "    border: none;"
+            "    color: white;"
+            "    padding: 3px 3px;"
+            "    text-align: center;"
+            "    text-decoration: none;"
+            "    font-size: 14px;"
+            "    margin: 4px 2px;"
+            "    border-radius: 10px;"
+            "}"
+            ""
+            "QPushButton:hover {"
+            "    background-color: #3e8e41;"
+            "}";
+        QFont font("Segoe UI");
+        sendDataLabel->setFont(font);
+        receivedDataLabel->setFont(font);
+
+
+        sendDataLabel->setStyleSheet("font: bold 13px; color: black;");
+        receivedDataLabel->setStyleSheet("font: bold 13px; color: black;");
+        // Create a label widget and set its font to Noto Sans
+//        QFont font("Noto Sans");
+        DataOrderLabel->setFont(font);
+        BitOrderLabel->setFont(font);
+
+        DataOrderLabel->setStyleSheet("font: bold 13px; color: black;");
+        BitOrderLabel->setStyleSheet("font: bold 13px; color: black;");
+
+        sendDataEdit->setStyleSheet("font-weight: bold; border: 1px solid 868482; color: gray; background-color: white;");
+
+        dataOrderCombo->setStyleSheet("font-weight: bold; border: 1px solid 868482; color:#899499; background-color: white;");
+        bitOrderCombo->setStyleSheet("font-weight: bold; border: 1px solid 868482; color: #899499; background-color: white;");
+
+        sendButton->setStyleSheet(styleSheet2);
+        receiveButton->setStyleSheet(styleSheet2);
+
+        receivedDataBrowser->setStyleSheet("QTextBrowser { background-color: #E3E0DF; }");
+        receivedDataBrowser->setTextColor(Qt::darkGreen);
+
+
+
+
+//        spiWidget->setStyleSheet("QWidget { background-color: #f0f0f0; }"
+//                                 "QLineEdit, QTextBrowser { background-color: white; border: 1px solid #ccc; }"
+//                                 "QPushButton { background-color: #33b5e5; color: white; }");
+
+        // Connect the buttons to their corresponding functions
+        connect(sendButton, &QPushButton::clicked, this, [sendDataEdit, receivedDataBrowser](){
+            QString sendData = sendDataEdit->text();
+            // Code to send data using the SPI interface
+            QString receivedData = "Received data";
+            receivedDataBrowser->append(sendData);
         });
-
-        // Set the layout for the widget
-        widget->setLayout(layout);
+        connect(receiveButton, &QPushButton::clicked, this, [receivedDataBrowser](){
+            // Code to receive data using the SPI interface
+            QString receivedData = "Showing data";
+            receivedDataBrowser->append(receivedData);
+        });
 
     }
 
